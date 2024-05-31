2e511386e656ab0840fa37dcdc22919d46edd724
vikashkumar6207 <vktd828403@gmail.com>
2024-05-31 12:03:38 +0530

    complete

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
-  await fs.appendFile(fileName, fileContent);
+  await fs.writeFile(fileName, fileContent);
+  console.log("File Created and Added Content Successfully...");
 };
 writeFile(fileName, fileContent);
 
